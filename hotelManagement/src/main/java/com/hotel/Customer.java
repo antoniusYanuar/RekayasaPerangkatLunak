24947d5920c8222f7527b573c05942307267bc67
Antonius Yanuar <antoniuskristian2@gmail.com>
2020-03-16 11:06:37 +0700

    initial commit

---
+++ hotelManagement/src/main/java/com/hotel/Customer.java
@@ -0,0 +1,12 @@
+package com.hotel;
+
+import javax.persistence.Entity;
+
+public class Customer {
+    private int id;
+    private String noKtp;
+    private String nama;
+    private String noTelpon;
+    private String alamatEmail;
+
+}
