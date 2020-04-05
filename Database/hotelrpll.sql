5e9e44584bcb527d30856e8663ce953d8d5612dd
kevinsuwanda <44392458+kevinsuwanda@users.noreply.github.com>
2020-04-05 22:56:22 +0700

    Add files via upload

--- Database/hotelrpll.sql
+++ Database/hotelrpll.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 17 Mar 2020 pada 14.40
--- Versi Server: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: Apr 05, 2020 at 05:42 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `customer`
+-- Table structure for table `customer`
 --
 
 CREATE TABLE `customer` (
@@ -37,12 +37,39 @@ CREATE TABLE `customer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `customer`
+-- Dumping data for table `customer`
 --
 
 INSERT INTO `customer` (`id`, `ktp`, `nama`, `telpon`, `email`) VALUES
 (1, '3308101801000003', 'Siapa Kamu', '089888787544', 'siapakamu@gmail.com'),
-(2, '3326160101810021', 'Kamu Siapa', '087888675432', 'kamusiapa@gmail.com');
+(2, '3326160101810021', 'Kamu Siapa', '087888675432', 'kamusiapa@gmail.com'),
+(3, '3308101801000002', 'asep', '087698594525', 'dika@gmail.com'),
+(4, '', '', '', ''),
+(5, '3308101801000007', 'asep', '087587548756', 'dika@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `id_register` int(11) NOT NULL,
+  `ktp` varchar(16) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `telpon` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`id_register`, `ktp`, `nama`, `telpon`, `email`) VALUES
+(1, '3308101801000003', 'Siapa Kamu', '089888787544', 'siapakamu@gmail.com'),
+(2, '3326160101810021', 'Kamu Siapa', '087888675432', 'kamusiapa@gmail.com'),
+(3, '3308101801000007', 'asep', '087698594525', 'dika@gmail.com'),
+(4, '', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -55,6 +82,12 @@ ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`id_register`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +95,13 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `register`
+--
+ALTER TABLE `register`
+  MODIFY `id_register` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
