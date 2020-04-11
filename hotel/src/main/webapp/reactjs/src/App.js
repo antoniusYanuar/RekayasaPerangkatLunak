436a7bb32247e89d2d2f8e6fada3496a63b83a0c
Reinforced <suwandakevin89@gmail.com>
2020-04-11 12:15:31 +0700

    React

--- hotel/src/main/webapp/reactjs/src/App.js
+++ hotel/src/main/webapp/reactjs/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
 import NavBarComponent from "./component/NavBarComponent";
+import SignUp from "./component/SignUp"
+import Dashboard from "./component/Dashboard"
 
 function App() {
   return (
-    <div className="App">
-      <NavBarComponent />
-      <p>Welcome To Our Hotel</p>
-      <p>Welcome To Our Hotel</p>
+    <SignUp/>
+    // <div className="App">
+    //   <NavBarComponent />
+    //   <p>Welcome To Our Hotel</p>
+    //   <p>Welcome To Our Hotel</p>
 
-      <p>Welcome To Our Hotel</p>
-    </div>
+    //   <p>Welcome To Our Hotel</p>
+    // </div>
   );
 }
 
